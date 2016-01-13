484283b5b659a109d1af4115c1526469ebd84f78
iAmNathanJ <jacobs.123@gmail.com>
2016-01-13 14:51:02 -0500

    update install.sh: list dotfiles in array and iteratively backup & symlink

--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
+# Define all dotfiles
+dotfiles=( ".zshrc" ".gitignore_global" )
 
-# files = ( "~/test" "~/test2" )
-
-
-
-# Backup originals
+# Ceate backup directory
 mkdir -p ~/dotfiles_old
-[ ! -f ~/.zshrc ] || mv -f ~/.zshrc ~/dotfiles_old/.zshrc
-[ ! -f ~/.gitignore_global ] || mv -f ~/.gitignore_global ~/dotfiles_old/.gitignore_global
+
+for i in "${dotfiles[@]}"
+do
+  # if file exists, back it up
+  [ ! -f ~/$i ] || mv -f ~/$i ~/dotfiles_old/$i
+
+  # create symlink
+  ln -s $PWD/$i ~/$i
+done
+
+##################
+# Manual Version
+##################
+
+# Backup files
+# [ ! -f ~/.zshrc ] || mv -f ~/.zshrc ~/dotfiles_old/.zshrc
+# [ ! -f ~/.gitignore_global ] || mv -f ~/.gitignore_global ~/dotfiles_old/.gitignore_global
 
 # Create symlinks
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+# ln -s ~/dotfiles/.zshrc ~/.zshrc
+# ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
