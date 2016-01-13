beec835027df70aa3e417b0247cf28282d5e77cb
iAmNathanJ <jacobs.123@gmail.com>
2016-01-13 14:30:04 -0500

    update install.sh: if files exist, then backup

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 
+# files = ( "~/test" "~/test2" )
+
+
+
 # Backup originals
 mkdir -p ~/dotfiles_old
-mv -f ~/.zshrc ~/dotfiles_old/.zshrc
-mv -f ~/.gitignore_global ~/dotfiles_old/.gitignore_global
+[ ! -f ~/.zshrc ] || mv -f ~/.zshrc ~/dotfiles_old/.zshrc
+[ ! -f ~/.gitignore_global ] || mv -f ~/.gitignore_global ~/dotfiles_old/.gitignore_global
 
 # Create symlinks
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.gitignore ~/.gitignore
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
