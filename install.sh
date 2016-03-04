277a4a433f9c81c3e88bae232a3e74bfd6f0784f
iAmNathanJ <jacobs@heysparkbox.com>
2016-03-04 11:50:44 -0500

    updates

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Define all dotfiles
-dotfiles=( ".bashrc" ".zshrc" ".editorconfig" ".jshintrc" ".atom" ".gitconfig" ".gitignore_global" )
+dotfiles=( ".bashrc" ".zshrc" ".editorconfig" ".jshintrc" ".atom" ".gitconfig" ".gitignore_global" ".vim" ".vimrc")
 
 # Ceate backup directory
 echo "Creating backup directory..."
