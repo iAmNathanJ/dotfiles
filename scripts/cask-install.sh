6c8af7d457e7c618040d5b400454fe97239c66ea
Nate Jacobs <jacobs.123@gmail.com>
2021-11-23 10:52:44 -0500

    remove unneeded cask programs

--- scripts/cask-install.sh
+++ scripts/cask-install.sh
@@ -4,13 +4,10 @@ brew cask install visual-studio-code
 brew cask install spectacle
 brew cask install alfred
 brew cask install slack
-brew cask install flux
 brew cask install dropbox
 
 brew cask install google-chrome
 brew cask install firefox
-
-brew cask install rocket
 
 brew tap homebrew/cask-fonts
 brew cask install font-fira-code
