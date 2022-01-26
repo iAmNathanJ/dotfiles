31cc049aa7e1a503eb014345a7d1cf7db33762d4
iamnathanj <nj@nj.codes>
2022-01-26 16:06:26 -0500

    setup for private spin dotfiles

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-if [ "$SPIN" ]; then
-  export DOTFILES="$HOME/dotfiles"
+function baseSetup() {
   $PWD/scripts/prompt-install.sh
   $PWD/scripts/link-dotfiles.sh
-else
-  export DOTFILES="$HOME/code/dotfiles"
+}
+
+function personalMachineSetup() {
   $PWD/scripts/mac-setup.sh
   $PWD/scripts/brew-install.sh
   $PWD/scripts/cask-install.sh
-  $PWD/scripts/prompt-install.sh
-  $PWD/scripts/link-dotfiles.sh
+}
+
+if [ ! "$SPIN" ]; then
+  export DOTFILES="$HOME/code/dotfiles"
+  baseSetup()
+  personalMachineSetup()
 fi
