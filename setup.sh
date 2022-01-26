7db812b0fa55b9c8fae490dc487d0efb4abd2085
iamnathanj <nj@nj.codes>
2022-01-26 16:39:32 -0500

    wip fixing spin

--- setup.sh
+++ setup.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-function baseSetup() {
-  $PWD/scripts/prompt-install.sh
-  $PWD/scripts/link-dotfiles.sh
-}
-
-function personalMachineSetup() {
-  $PWD/scripts/mac-setup.sh
-  $PWD/scripts/brew-install.sh
-  $PWD/scripts/cask-install.sh
-}
-
 if [ ! "$SPIN" ]; then
   export DOTFILES="$HOME/code/dotfiles"
-  baseSetup()
-  personalMachineSetup()
+
+  echo "***********************************"
+  echo "WHY?"
+  echo "***********************************"
+
+  $PWD/scripts/prompt-install.sh
+  $PWD/scripts/link-dotfiles.sh
+  # $PWD/scripts/mac-setup.sh
+  # $PWD/scripts/brew-install.sh
+  # $PWD/scripts/cask-install.sh
 fi
+
+# git config --global user.name iAmNathanJ
